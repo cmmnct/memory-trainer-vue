89f83d9c2aa3d91c723a52491b30e854e19f441a
CMMNCT <mark@cmmnct.nl>
2024-08-05 13:29:56 +0200

    deploy

--- .firebase/memory-cmmnct-nl/hosting/assets/ios.transition-legacy-BKpXEFuO.js
+++ .firebase/memory-cmmnct-nl/hosting/assets/ios.transition-legacy-BKpXEFuO.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CqnGG7kI.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.b,n=t.g}],execute:function(){
+System.register(["./index-legacy-CEwaWMK6.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.b,n=t.g}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
